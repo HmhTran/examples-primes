a4e1816bf506afe3e3f3b849b05d4f06ef6f6e0c
hmhtran <harry.tran@mail.mcgill.ca>
2013-10-04 01:27:20 -0400

    Change name of variable printPrimes to example

--- PrintPrimes.java
+++ PrintPrimes.java
@@ -15,7 +15,7 @@ public class printPrimes {
 
 
   public static void main(String[] args) {
-      printPrimes printPrimes = new printPrimes(300, 50, 4, 10, 30);
+      printPrimes example = new printPrimes(300, 50, 4, 10, 30);
       printPrimes.calculatePrimes();
       printPrimes.printPrimes();
   }
